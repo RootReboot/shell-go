14e80cb0b7b10375b27fa08ca9682e66da3ede1a
rootreboot <root@reboot>
2025-06-16 22:50:54 +0100

    REPL

--- app/main.go
+++ app/main.go
@@ -10,12 +10,15 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	for true {
 
-	fmt.Println(command[:len(command)-1] + ": command not found")
+		fmt.Fprint(os.Stdout, "$ ")
 
+		// Wait for user input
+		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
+
+	}
 }
